58d94a5cb159c77ae61b8a92597d0f646de40a81
Mauricio Caserta <mauriciocaserta@gmail.com>
2019-01-05 12:15:46 -0200

    Criação do esqueleto da tela para criar novo lembrete

--- app/js/index.js
+++ app/js/index.js
@@ -6,14 +6,14 @@ const btnMyReminders = document.getElementById('btnMyReminders')
 // Clique no botão do github
 btnOpenGit.addEventListener('click', function() {
     ipcRen.send('btnOpenGit', btnOpenGit.getAttribute('data-href'))
-});
+})
 
 // Clique no botão novo lembrete
 btnNewReminder.addEventListener('click', function() {
     ipcRen.send('btnNewReminder')
-});
+})
 
 // Clique no botão meus lembretes
 btnMyReminders.addEventListener('click', function() {
     ipcRen.send('btnMyReminders')
-});
+})
--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 const { app, BrowserWindow, ipcMain } = require('electron')
 var path = require('path')
-var shell = require('electron').shell;
+var shell = require('electron').shell
+
+var mainWindow
 
 function createWindow() {
+
     // Criar o componente da tela
     mainWindow = new BrowserWindow({ width: 300, height: 500, resizable: false, icon: path.join(__dirname, 'app/icon/reminder.ico') })
 
@@ -16,14 +19,15 @@ app.on('ready', createWindow)
 
 // Abrir a URL do Git
 ipcMain.on('btnOpenGit', function (event, arg) {
-    event.preventDefault();
-    shell.openExternal(arg);
-});
+    event.preventDefault()
+    shell.openExternal(arg)
+})
 
 // Novo lembrete
 ipcMain.on('btnNewReminder', function (event, arg) {
-});
+    mainWindow.loadURL(`file://${__dirname}/app/html/new-reminder.html`)
+})
 
 // Meus lembretes
 ipcMain.on('btnMyReminders', function (event, arg) {
-});
+})
