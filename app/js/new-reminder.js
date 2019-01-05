71b376a8604ce5982ed35c8fa79341c829c57533
Mauricio <mauriciocaserta@gmail.com>
2019-01-05 16:26:33 -0200

    Criação de tabela no SQLite

--- app/js/new-reminder.js
+++ app/js/new-reminder.js
@@ -1,13 +1,38 @@
 const ipcRen = require('electron').ipcRenderer
 const btnCreate = document.getElementById('btnCreate')
 const btnCancel = document.getElementById('btnCancel')
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('./lembrett.sql');
 
 // Clique no botão para criar novo lembrete
 btnCreate.addEventListener('click', function() {
-    ipcRen.send('btnCreate')
+    insertNewReminder("teste", 123)
 })
 
 // Clique no botão para cencelar
 btnCancel.addEventListener('click', function() {
     ipcRen.send('btnCancel')
 })
+
+
+function insertNewReminder(description, date){
+    let stmt = db.prepare("INSERT INTO reminder (description, date_remind) VALUES (?,?)")
+    stmt.run(description, date, function(){
+        console.log("Novo lembrete criado com sucesso!")
+        launch_toast()
+    })
+}
+
+function listReminders(){
+    db.each("SELECT * FROM reminder", function(err, row) {
+    });
+}
+
+function launch_toast() {
+    var x = document.getElementById("toast")
+    x.className = "show";
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 2000);
+    setTimeout(function() {
+        ipcRen.send('btnCancel')
+    }, 2000)
+}
--- app/js/sqlite.js
+++ app/js/sqlite.js
@@ -1,19 +1,8 @@
 var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('./database.sqlite3');
+var db = new sqlite3.Database('./lembrett.sql');
  
 db.serialize(function() {
-    console.log(" CREATING TABLE");
-  db.run("CREATE TABLE lorem (info TEXT)");
- 
-  var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-  for (var i = 0; i < 10; i++) {
-      stmt.run("Ipsum " + i);
-  }
-  stmt.finalize();
- 
-  db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
-      console.log(row.id + ": " + row.info);
-  });
+  db.run("CREATE TABLE reminder (id PRIMARY KEY, description TEXT, date_remind TEXT)")
 });
  
 db.close();
