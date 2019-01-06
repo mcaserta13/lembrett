fc2c08956de8ba541cb2fc9fca01c54d0b9aeaf2
Mauricio Caserta <mauriciocaserta@gmail.com>
2019-01-06 16:17:45 -0200

    Ajustes para criar jobs existentes

--- app/js/sqlite.js
+++ app/js/sqlite.js
@@ -2,7 +2,7 @@ const sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('./lembrett.sql');
  
 db.serialize(function() {
-  db.run('CREATE TABLE reminder (id PRIMARY KEY, description TEXT, date_remind TEXT, horary_remind TEXT, repeat INT, read INT)')
+  db.run('CREATE TABLE IF NOT EXISTS reminder (id PRIMARY KEY, description TEXT, date_remind TEXT, horary_remind TEXT, repeat INT, read INT)')
 });
  
 db.close();
--- main.js
+++ main.js
@@ -12,15 +12,20 @@ var mainWindow
 function createWindow() {
 
     // Criar o componente da tela
-    mainWindow = new BrowserWindow({ width: 350, height: 620, resizable: true, icon: path.join(__dirname, 'app/icon/reminder.ico') })
+    mainWindow = new BrowserWindow({ width: 350, height: 620, resizable: true, icon: path.join(__dirname, 'app/icon/reminder.png') })
 
     mainWindow.setMenu(null)
 
-    // Limpar todos lembretes antigos que não repetem
-    cleanOldReminders()
+    // Verificar se a tabela já existe
+    tableExists(function(exists) {
+        if (exists) {
+            // Limpar todos lembretes antigos que não repetem
+            cleanOldReminders()
 
-    // Criar as tarefas scheduladas salvas no banco de dados
-    createSchedules()
+            // Criar as tarefas scheduladas salvas no banco de dados
+            createSchedules()
+        }
+    });
 
     // Carregar o arquivo index.html
     mainWindow.loadFile('index.html')
@@ -80,7 +85,7 @@ async function notify(description, id) {
         title: 'Chegou a hora!',
         message: description,
         priority: 2,
-        icon: './app/icon/reminder.ico'
+        icon: './app/icon/reminder.png'
     });
 
     if (id > 0) {
@@ -91,27 +96,31 @@ async function notify(description, id) {
 
 // Criar as notificações scheduladas de acordo com o banco de dados
 async function createSchedules() {
-    db.each('SELECT * FROM reminder', function (err, row) {
-        var id = row.id
-        var description = row.description
-        var date = row.date_remind
-        var horary = row.horary_remind
-        var repeat = row.repeat
+    try {
+        db.each('SELECT * FROM reminder', function (err, row) {
+            var id = row.id
+            var description = row.description
+            var date = row.date_remind
+            var horary = row.horary_remind
+            var repeat = row.repeat
 
-        if (repeat <= 0) {
-            var scheduledDate = new Date(date + ' ' + horary)
-            console.log('Lembrete schedulado em : ' + scheduledDate)
-            scheduler.scheduleJob(scheduledDate, function () {
-                notify(description, id)
-            });
-        } else {
-            var splittedHorary = horary.split(':')
-            console.log("Lembrete schedulado diariamente as " + horary)
-            scheduler.scheduleJob(splittedHorary[1] + ' ' + splittedHorary[0] + ' * * *', function () {
-                notify(description, 0)
-            })
-        }
-    });
+            if (repeat <= 0) {
+                var scheduledDate = new Date(date + ' ' + horary)
+                console.log('Lembrete schedulado em : ' + scheduledDate)
+                scheduler.scheduleJob(scheduledDate, function () {
+                    notify(description, id)
+                });
+            } else {
+                var splittedHorary = horary.split(':')
+                console.log("Lembrete schedulado diariamente as " + horary)
+                scheduler.scheduleJob(splittedHorary[1] + ' ' + splittedHorary[0] + ' * * *', function () {
+                    notify(description, 0)
+                })
+            }
+        })
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 // Remover um lembrete do banco de dados de acordo com o id
@@ -124,8 +133,24 @@ async function removeScheduled(id) {
 
 // Remover lembretes antigos
 function cleanOldReminders() {
-    let stmt = db.prepare("DELETE FROM reminder WHERE repeat = 0 AND (date('now') > date(date_remind) AND time('now', '-2 hours') > time(horary_remind)) OR (date('now') = date(date_remind) AND time('now', '-2 hours') > time(horary_remind))")
-    stmt.run(function (e, row) {
-        console.log(row)
+    try {
+        let stmt = db.prepare("DELETE FROM reminder WHERE repeat = 0 AND (date('now') > date(date_remind) AND time('now', '-2 hours') > time(horary_remind)) OR (date('now') = date(date_remind) AND time('now', '-2 hours') > time(horary_remind))")
+        stmt.run(function (e, row) {
+            console.log("Lembretes antigos removidos com sucesso!")
+        })
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+
+// Verificar se a tabela existe no banco de dados
+tableExists = function (callback){
+    db.get("SELECT name FROM sqlite_master WHERE type='table' AND name='reminder'", function (err, row) {
+        if (err != null || row === undefined) {
+            callback(false)
+        } else {
+            callback(true)
+        }
     })
 }
